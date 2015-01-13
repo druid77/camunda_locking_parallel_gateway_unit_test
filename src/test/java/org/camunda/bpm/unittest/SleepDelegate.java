d5d75118891e3c3cb45acb2f0c0f59362ddeefdb
Galen A Hollins <Galen.A.Hollins@jpl.nasa.gov>
2015-01-13 14:14:08 -0800

    updates

--- src/test/java/org/camunda/bpm/unittest/SleepDelegate.java
+++ src/test/java/org/camunda/bpm/unittest/SleepDelegate.java
@@ -6,7 +6,6 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 public class SleepDelegate implements JavaDelegate {
 	
 	public void execute(DelegateExecution execution) throws Exception {
-		// TODO Auto-generated method stub
 		System.out.println(Thread.currentThread() + " -- LockingDelegate is running");
 	}
 
